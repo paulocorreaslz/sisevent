cab6ebc18b7af046f7d3f983012c300dda16b62c
Paulo Correa <pauloyaco@gmail.com>
2019-06-27 15:53:54 -0300

    initial commit.

---
+++ src/main/java/com/paulocorreaslz/sisevent/SiseventApplication.java
@@ -0,0 +1,13 @@
+package com.paulocorreaslz.sisevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiseventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiseventApplication.class, args);
+	}
+
+}
