c961ab71ccbf3a7fc2508520650c851d95ab5c1e
Paulo Correa <pauloyaco@gmail.com>
2019-07-13 16:09:14 -0300

    add port and crossorigin

--- src/main/java/com/paulocorreaslz/sisevent/controller/CustomerController.java
+++ src/main/java/com/paulocorreaslz/sisevent/controller/CustomerController.java
@@ -32,7 +32,7 @@ import com.paulocorreaslz.sisevent.repo.AddressRepository;
 import com.paulocorreaslz.sisevent.repo.CustomerRepository;
 import com.paulocorreaslz.sisevent.reports.CustomerReport;
  
-@CrossOrigin(origins = {"http://localhost:4200","http://localhost"})
+@CrossOrigin(origins = {"http://localhost:4200","http://localhost","https://sisevent.herokuapp.com/"})
 @RestController
 @RequestMapping("/api")
 public class CustomerController {
